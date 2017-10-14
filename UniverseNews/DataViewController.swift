86b02e298aa6d1bab4acf67e01b63ac055c6604f
firewood <karamaki@gmail.com>
2017-10-14 16:29:43 +0900

    add WKWebView

--- UniverseNews/DataViewController.swift
+++ UniverseNews/DataViewController.swift
@@ -7,16 +7,33 @@
 //
 
 import UIKit
+import WebKit
 
 class DataViewController: UIViewController {
 
     @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var usageLabel: UILabel!
     var dataObject: String = ""
+    var webView: WKWebView!
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        webView = WKWebView()
+        let url:URL! = URL(string:"https://www.google.com")
+        webView.load(URLRequest(url: url))
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        webView.frame = view.frame
+        webView.frame.origin.x = dataLabel.frame.origin.x
+        webView.frame.origin.y += dataLabel.frame.origin.y + dataLabel.frame.height;
+        webView.frame.size.width -= dataLabel.frame.origin.x * 2;
+        webView.frame.size.height -= 120
+        view.addSubview(webView)
+        updateUsage()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +46,9 @@ class DataViewController: UIViewController {
         self.dataLabel!.text = dataObject
     }
 
-
+    func updateUsage() {
+        let r = URLCache.shared.currentDiskUsage
+        usageLabel.text = String(format: "%ld", r)
+    }
 }
 
