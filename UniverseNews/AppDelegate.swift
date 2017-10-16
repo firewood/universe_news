de5663220988e1bb590402c4d965b1247774774e
firewood <karamaki@gmail.com>
2017-10-17 01:13:03 +0900

    dump simulator path

--- UniverseNews/AppDelegate.swift
+++ UniverseNews/AppDelegate.swift
@@ -13,9 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        print(NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0])
         return true
     }
 
@@ -27,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//        URLCache.shared.removeAllCachedResponses()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -41,6 +41,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
